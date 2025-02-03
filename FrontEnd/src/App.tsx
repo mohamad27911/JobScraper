8985195e11783015017c9efdf52e3e1ab6cc060f
mohamad27911 <mohamad.mar72@gmail.com>
2025-02-03 22:40:25 +0200

    Project setup React + FastApi

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl font-bold underline text-center min-h-screen m-auto mt-10 text-[40px] text-red-500">
+    Hello world!
+  </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
