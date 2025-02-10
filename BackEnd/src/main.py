1fb8878ca76c2338cc1484f864e308467bd42528
mohamad27911 <mohamad.mar72@gmail.com>
2025-02-10 22:14:51 +0200

    allow frontend to access 2

--- BackEnd/src/main.py
+++ BackEnd/src/main.py
@@ -22,7 +22,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can specify the exact domains, like ["https://yourdomain.com"]
+    allow_origins=["https://job-scraping-mohamad27911s-projects.vercel.app"],  # You can specify the exact domains, like ["https://yourdomain.com"]
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
